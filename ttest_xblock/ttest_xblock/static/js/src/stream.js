1696664e8d9aa44d3234d2ced25e2e01871e8718
calibretaliation <nhatdtm1902@gmail.com>
2022-08-23 13:31:06 +0700

    update response information

--- ttest_xblock/ttest_xblock/static/js/src/stream.js
+++ ttest_xblock/ttest_xblock/static/js/src/stream.js
@@ -1,65 +1,58 @@
 function TestXBlock(runtime, element) {
-console.log(JSON.stringify(runtime));
-console.log(JSON.stringify(element));
-console.log(JSON.stringify($('button', element)))
-const videoElem = document.getElementById('video')
 
+const originalVideoElem = document.getElementById('ovideo')
 var startBtn = document.getElementById('start-record')
 var endBtn = document.getElementById('stop-record')
+
 function SuccessUpdate(result) {
-    window.alert(result.value);
+    if (result.state == 1) {
+        document.getElementById('state').textContent = "DROWSY";
+    }
+    else {
+        document.getElementById('state').textContent= "NOT DROWSY";
+    }
 }
+
 var recorder;
 
 const settings = {
-video: true,
-audio: true
+    video: true,
+    audio: true
 }
 
 startBtn.addEventListener('click', function (e) {
     navigator.mediaDevices.getUserMedia(settings).then((stream) => {
         console.log(stream);
-        videoElem.srcObject = stream
+        originalVideoElem.srcObject = stream
 
         recorder = new MediaRecorder(stream)
         console.log(recorder);
 
         recorder.start();
 
-        const blobContainer = [];
+        var blobContainer = [];
 
         recorder.ondataavailable = function (e) {
             blobContainer.push(e.data)
+            console.log("data chunk: ", e.data.size)
         }
+        window.setInterval(function() {
+            recorder.stop();
+            recorder.start();
 
-        recorder.onerror = function (e) {
-            return console.log(e.error || new Error(e.name));
-        }
+        }, 100)
+        window.setInterval(function(){
+            console.log(blobContainer)
+            console.log(blobContainer.length)
+            var video = new Blob(blobContainer.splice(blobContainer.length - 1, 1), { type: 'video/mp4'});
+            console.log(video)
 
-        
-
-
-        recorder.onstop = function (e) {
-
-            var video = new Blob(blobContainer, { type: 'video/webm'});
-            console.log(video);
             var streamUrl = runtime.handlerUrl(element, 'receive_video');
-            // var a = document.createElement('a');
-            // a.download = 'download.webm';
-            // a.href = window.URL.createObjectURL(video);
-            // console.log(a.href);
-            // a.click();
-            // var Mydata = {"file": a.href};  
             var reader = new FileReader();
             reader.readAsDataURL(video);
             reader.onload = function() {
                 var Mydata_2 = {"file": reader.result};
-                console.log(reader.result.length);
-                console.log(reader.result);
-                var a = document.createElement('a');
-                a.download = 'download.txt';
-                a.href = window.URL.createObjectURL(new Blob([JSON.stringify(Mydata_2)]));
-                a.click();
+                console.log("video blob: ", reader.result);
                 $.ajax({
                     type: "POST",
                     url: streamUrl,
@@ -69,25 +62,26 @@ startBtn.addEventListener('click', function (e) {
                     success: SuccessUpdate
                 }); 
             }
-            
-            // $.ajax({
-            //     type: "POST",
-            //     url: streamUrl,
-            //     data: JSON.stringify(Mydata_2),
-            //     processData: false,
-            //     contentType: false,
-            //     success: SuccessUpdate
-            // }); 
+
+            blobContainer = []
+        }, 1001)
+
+        recorder.onerror = function (e) {
+            return console.log(e.error || new Error(e.name));
         }
     })
-    
+
+
+
 })
-
-
 
 endBtn.addEventListener('click', function (e) {
-    videoElem.pause();
+    originalVideoElem.pause();
     recorder.stop();
-    
+    navigator.mediaDevices.getUserMedia(settings).then((stream) => {
+    stream.getTracks().forEach( track => track.stop() ); 
+    })
 })
+
+
 }
--- ttest_xblock/ttest_xblock/ttest_xblock.py
+++ ttest_xblock/ttest_xblock/ttest_xblock.py
@@ -3,12 +3,12 @@
 import base64
 import io
 import json
+import cv2
 from django.shortcuts import render
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
 from xblock.fields import Integer, Scope, Boolean, String 
-import cv2
 from django.http import HttpResponse, StreamingHttpResponse
 from django.urls import path
 from django.core.files.storage import FileSystemStorage
@@ -17,60 +17,66 @@ from werkzeug.utils import secure_filename
 #Change import in flask_uploads.py
 from flask import Flask, jsonify, request
 import requests
-
+from tracking.Tracking.eye_tracking import  run_eye_check
 class TestXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
-    upvotes = Integer(help="Number of up votes", default=0,
-        scope=Scope.user_state_summary)
-    downvotes = Integer(help="Number of down votes", default=0,
-        scope=Scope.user_state_summary)
-    voted = Boolean(help="Has this student voted?", default=False,
+    sleepy_state = Integer(help="SLEEPY STATE", default=0,
         scope=Scope.user_state)
-    streaming = String(help = "Streaming image", default="",
-        scope=Scope.user_state_summary)
+    student_id = Integer(help = "Student ID")
+    count = Integer(help = "count of video", default=0,
+        scope=Scope.user_state)
+    student_count = Integer(help = "total number of student", default = 0, 
+        scope = Scope.user_state_summary)
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
         The primary view of the TestXBlock, shown to students
         when viewing courses.
         """
+        self.student_count += 1
+        self.student_id = self.student_count
+        self.count = 0
         html = self.resource_string("static/html/index.html")
         frag = Fragment(html.format(self=self))
         frag.add_javascript(self.resource_string("static/js/src/stream.js"))
         frag.initialize_js('TestXBlock')
         return frag
+
+    def teacher_view(self, context = None):
+        html = self.resource_string("static/html/index.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_javascript(self.resource_string("static/js/src/stream.js"))
+        frag.initialize_js('TestXBlock')
+
+        return frag
     @XBlock.json_handler
     def receive_video(self, data, suffix=''):
-            video_content = data['file'][23:]
-            #fs.save('video.mp4', myfile)
+            video_content = data['file'][22:]
             print(len(data['file']))
+
             print(type(data['file']))
             decoded_string = base64.b64decode(video_content) 
-
-            with open('video.webm', 'wb') as wfile:
+            video_name = f"video_{self.student_id}_{self.count}.mp4"
+            with open(video_name, 'wb') as wfile:
                 wfile.write(decoded_string)
+            self.count += 1
+            self.sleepy_state = run_eye_check(video_name)
+            print(self.sleepy_state)
+            if  self.sleepy_state == 1:
+                # state_image = cv2.imread("drowsy_detect.jpg")
+                # image_string = base64.b64encode(state_image)
+                return{"state": f"{ self.sleepy_state}"}
+                                # "image":f"{image_string}"}
+            elif  self.sleepy_state == 0:
+                return {"state": f"{ self.sleepy_state}"}
 
-            # print(json.loads(video_content))
-            with open('video.txt', 'w') as f_vid:
-                f_vid.write(video_content)
-            return {"value": "1"}
-            # uploaded_file_url = fs.url(filename)
 
     @staticmethod
     def workbench_scenarios():
@@ -80,3 +86,4 @@ class TestXBlock(XBlock):
              """<ttest_xblock/>
              """),
         ]
+
